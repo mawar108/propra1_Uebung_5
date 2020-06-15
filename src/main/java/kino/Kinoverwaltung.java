9f2bf9cb6f47b5c03e814001bb59d292d9373e89
msurl <32799737+msurl@users.noreply.github.com>
2020-06-15 12:54:09 +0200

    Initial commit

---
+++ src/main/java/kino/Kinoverwaltung.java
@@ -0,0 +1,11 @@
+package kino;
+
+public class Kinoverwaltung {
+
+    public static void main(String[] args) {
+        // Demonstrieren Sie hier Ihr System,
+        // indem Sie mehrere Reservierungen vornehmen
+        // Es müssen auch sich überlappende Reservierungen vorkommen!
+    }
+
+}
